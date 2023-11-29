cedb645395f28b9c178a166336057afe2604d781
Cubites <cubite404@gmail.com>
2023-11-29 19:11:50 +0900

    CREATE: 프로젝트 생성

---
+++ waglewagle/src/main/java/kr/co/waglewagle/config/WebConfig.java
@@ -0,0 +1,93 @@
+package kr.co.waglewagle.config;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@ComponentScan(basePackages = "kr.co.waglewagle")
+@EnableWebMvc
+@MapperScan(basePackages = "kr.co.waglewagle", annotationClass = Mapper.class)
+public class WebConfig implements WebMvcConfigurer {
+
+	@Value("${db.driver}")
+	private String driver;
+	@Value("${db.url}")
+	private String url;
+	@Value("${db.username}")
+	private String username;
+	@Value("${db.userpassword}")
+	private String userpassword;
+
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver rs = new InternalResourceViewResolver();
+		rs.setPrefix("/WEB-INF/views/");
+		rs.setSuffix(".jsp");
+		registry.viewResolver(rs);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	// db관련 bean등록
+	@Bean(destroyMethod = "close")
+	public HikariDataSource dataSource() {
+		HikariDataSource dataSource = new HikariDataSource();
+
+		dataSource.setDriverClassName(driver);
+		dataSource.setJdbcUrl(url);
+		dataSource.setUsername(username);
+		dataSource.setPassword(userpassword);
+		return dataSource;
+	}
+
+	// mybatis
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+
+		SqlSessionFactoryBean ssf = new SqlSessionFactoryBean();
+		ssf.setDataSource(dataSource());
+		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+		ssf.setMapperLocations(resolver.getResources("classpath:/*/**.xml"));
+		return ssf.getObject();
+	}
+
+	// sqlSessionFactory
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate() throws Exception {
+		return new SqlSessionTemplate(sqlSessionFactory());
+	}
+
+	// 프로퍼티설정 파일 읽기
+	@Bean
+	public static PropertyPlaceholderConfigurer property() {
+		PropertyPlaceholderConfigurer pro = new PropertyPlaceholderConfigurer();
+		pro.setLocation(new ClassPathResource("db.properties"));
+		return pro;
+	}
+
+}
