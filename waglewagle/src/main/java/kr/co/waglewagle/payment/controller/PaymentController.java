88ea537514618191d83b4efd7893066ea46f8f50
SJJ <ddelsen3130@gmail.com>
2023-12-14 18:57:41 +0900

    Design: 포인트 충전 페이지 추가 & Feat: 결제 창 실행 추가

--- waglewagle/src/main/java/kr/co/waglewagle/payment/controller/PaymentController.java
+++ waglewagle/src/main/java/kr/co/waglewagle/payment/controller/PaymentController.java
@@ -2,6 +2,8 @@ package kr.co.waglewagle.payment.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import kr.co.waglewagle.payment.service.PaymentService;
 
@@ -11,4 +13,13 @@ public class PaymentController {
 	@Autowired
 	private PaymentService service;
 	
+	// 포인트 충전 페이지로 이동
+	@GetMapping("/payment")
+	public String goPayment() {
+		return "payment/payment";
+	}
+	
+//	// 포인트 충전
+//	@PostMapping("/payment")
+//	public String 
 }
